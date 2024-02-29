cbe300bc6955e292f464387f1fa46362e7615d28
Dalibor Babić <124865552+laddeeth@users.noreply.github.com>
2024-02-29 10:07:18 +0100

    Minor fix

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,6 +1,6 @@
 const ToDo = ({ task, method, index, deleteMethod, id }) => {
   return (
-    <li className={!(index % 2) && 'odd'}>
+    <li className={index % 2 ? '' : 'odd'}>
       {task}
       <button onClick={method}>Done</button>
       <button onClick={() => deleteMethod(id)}>Delete</button>
